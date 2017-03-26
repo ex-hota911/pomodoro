4c528d84168f59dcf149fcb0a4d138baf878d71b
Hiroyuki Ota <hiroyuki.ot@gmail.com>
2017-03-27 01:55:07 +0900

    Show work and break

--- pomo.js
+++ pomo.js
@@ -12,151 +12,97 @@ function TimeComp(props) {
   );
 };
 
-class TimeEditor extends React.Component {
-  constructor() {
-	super();
-	this.state = {value: 25};
-  }
-  reset() {
-  }
-  handleChange(event) {
-    this.setState({value: parseInt(event.target.value, 10)});
-  }
-  render() {
-	return (
+TimeComp.propTypes = {
+  // Time in milli sec.
+  time : React.PropTypes.number
+}
+
+/**
+ * 
+ */
+function TimeEditor(props) {
+  return (
 	  <div>
-		<input type="number" value={this.state.value} onChange={(e) => this.handleChange(e)}/> min
-        <button onClick={() => this.props.onReset(this.state.value)}>
-		  Reset
-	    </button>
-        <button onClick={() => this.props.onReset(0.05)}>
-		  3 sec
+   	    Work:
+	    <input type="number" value={props.workTime} onChange={(e) => props.setWork(e.target.value)}/> min
+	    Break:
+	    <input type="number" value={props.breakTime} onChange={(e) => props.setBreak(e.target.value)}/> min
+        <button onClick={() => props.onReset()}>
+          Reset
 	    </button>
 	  </div>
 	)
-  }
 }
 
-// props
-// {number} time Count down original time in sec.
-// state
-// {number} remaningTime Count down time from the original in millis.
-// {?number} start When the timer started.
-// {?number} interval The ID of setInterval.
-class Timer extends React.Component {
-  constructor(props) {
-	super(props);
-	this.state = {
-	  remaningTime: this.props.time * 1000,
-	};
-  }
-  reset() {
-	this.setState({
-	  remaningTime: this.props.time * 1000,
-	  start: null,
-	  interval: null,
-	});
+TimeEditor.propTypes = {
+  workTime: React.PropTypes.string,
+  breakTime: React.PropTypes.string,
+  onReset: React.PropTypes.func,
+  setWork: React.PropTypes.func,
+  setBreak: React.PropTypes.func,
+}
+
+class Timer {
+  constructor(time, onTick, onFinish) {
+	this.start = null;
+	this.interval = null;
+	this.onTick = onTick;
+	this.onFinish = onFinish;
+	this.remainingTime = time;
   }
   tick() {
 	var time = this.getTime();
 	if (time == 0) {
 	  this.stopTimer();
-	  
-	  notify(() => this.reset());
-	  return;
+	  this.onFinish();
+	} else {
+	  this.onTick(time, this.isRunning());
 	}
-	this.forceUpdate();
   }
   getTime() {
 	if (this.isRunning()) {
-	  return Math.max(this.state.start + this.state.remaningTime - new Date().getTime(), 0);
+	  return Math.max(this.start + this.remainingTime - new Date().getTime(), 0);
 	} else {
-	  return this.state.remaningTime;
+	  return this.remainingTime;
 	}
   }
-  toggleTimer() {
-	if (this.state.interval) {
-	  this.stopTimer();
-	} else {
-	  this.startTimer();
+  setTime(time) {
+	if (this.isRunning()) {
+	  console.error('Timer is running');
+	  return;
 	}
+	this.remainingTime = time;
   }
   startTimer() {
-	let interval = window.setInterval(() => {this.tick();}, 1000);
-	var state = {
-	  interval: interval,
-	  start: new Date().getTime()
-	};
-	if (this.state.remaningTime == 0) {
-	  state.remaningTime = this.props.time * 1000;
+	console.log(this.remainingTime);
+	if (this.isRunning()) {
+	  console.error('Timer is already started');
+	  return;
 	}
-	this.setState(state);
+	if (this.getTime() == 0) {
+	  console.error('Timer is already finished');
+	  this.onFinish();
+	  return;
+	}
+	this.interval = window.setInterval(() => {this.tick();}, 1000);
+	this.start = new Date().getTime();
+
+	this.onTick(this.getTime(), true);
   }
   stopTimer() {
-	if (!this.isRunning()) return;
-	window.clearInterval(this.state.interval);
-	var time = this.getTime();
-	this.setState({
-	  remaningTime: time,
-	  interval: null,
-	  start: null
-	});
+	if (!this.isRunning()){
+	  console.error('Timer is already stopped');
+	  return;
+	}
+	this.remaningTime = this.getTime();
+	window.clearInterval(this.interval);
+	this.interval = null;
+	this.start = null;
+
+	this.onTick(this.remaningTime, false);
   }
   isRunning() {
-	return !!this.state.interval;
-  }
-  componentWillReceiveProps(nextProps) {
-	this.stopTimer();
-	this.setState({remaningTime : nextProps.time * 1000})
-  }
-  render() {
-	var message = (this.state.interval)? 'Stop timer':'Start timer';
-	var time = this.getTime();
-	// Hack
-	return (
-	  <div>
-		<TimeComp time={time}/>
-		<button onClick={() => this.toggleTimer()}>
-		  {message}
-		</button>
-      </div>
-	);
-  }
-}
-
-// props
-// {number} time Count down original time in sec.
-// state
-// {number} remaningTime Count down time from the original in millis.
-// {?number} start When the timer started.
-// {?number} interval The ID of setInterval.
-class Timer2 extends React.Component {
-  constructor(props) {
-	super(props);
-	this.timer_ = new Timer(this.props.time, () => this.forceUpdate());
-  }
-  toggleTimer() {
-	if (this.timer_.isRunning()) {
-	  this.timer_.stopTimer();
-	} else {
-	  this.timer_.startTimer();
-	}
-  }
-  componentWillReceiveProps(nextProps) {
-	this.timer_.stopTimer();
-	this.setState({remaningTime : nextProps.time * 1000})
-  }
-  render() {
-	var message = this.timer_.isRunning() ? 'Stop timer':'Start timer';
-	var time = this.timer_.getTime();
-	return (
-	  <div>
-		<TimeComp time={time}/>
-		<button onClick={() => this.toggleTimer()}>
-		  {message}
-		</button>
-      </div>
-	);
+	return !!this.interval;
   }
 }
 
@@ -164,21 +110,131 @@ class App extends React.Component {
   constructor() {
 	super();
 	this.state = {
-	  time : 25 * 60
+	  workTime: 25,
+	  breakTime: 5,
+	  formWork: '-3',
+	  formBreak: '-2',
+	  timeInMillis: 25 * 60 * 1000,
+	  isWork: true,
+	  running: false,
 	};
+	this.timer = new Timer(this.state.timeInMillis, this.tick.bind(this), this.onFinish.bind(this));
   }
+
+  setState(state) {
+	React.Component.prototype.setState.call(this, state);
+	console.log(state);
+  }
+
+  tick(time, isRunning) {
+	this.setState({
+	  timeInMillis: time,
+	  running: isRunning,
+	})
+  }
+
+  onFinish() {
+	var time;
+	if (this.state.isWork) {
+	  time = this.setBreak(this.state.breakTime);
+	} else {
+	  time = this.setWork(this.state.workTime);
+	}
+	this.setState({running: false});
+	notify(() => {
+	  // There is a race condition.
+	  this.timer.startTimer();
+	});
+  }
+
+  setWork(workTime) {
+	var timeInMillis = workTime * 60 * 1000;
+	// For debugging.
+	if (workTime < 0) {
+	  timeInMillis = workTime * -1000;
+	}
+	this.setState({
+	  isWork: true,
+	  timeInMillis: timeInMillis
+	});
+	this.timer.setTime(timeInMillis);
+	return timeInMillis;
+  }
+  setBreak(breakTime) {
+	var timeInMillis = breakTime * 60 * 1000;
+	// For debugging.
+	if (breakTime < 0) {
+	  timeInMillis = breakTime * -1000;
+	}
+	this.setState({
+	  isWork: false,
+	  timeInMillis: timeInMillis
+	});
+	this.timer.setTime(timeInMillis);
+	return timeInMillis;
+  }
+  startWork() {
+	if (this.state.running) {
+	  this.timer.stopTimer();
+	}
+	this.setWork(this.state.workTime);
+	this.timer.startTimer();
+  }
+
+  pauseResumeClicked() {
+	if (this.state.running) {
+	  this.timer.stopTimer();
+	} else {
+	  this.timer.startTimer();
+	}
+  }
+
+  reset() {
+	this.timer.stopTimer();
+	var workTime = parseInt(this.state.formWork, 10);
+	var breakTime = parseInt(this.state.formBreak, 10);
+	this.setState({
+	  workTime: workTime,
+	  breakTime: breakTime,
+	});
+	this.setWork(workTime);
+  }
+
+  setStateKeyValue(key, value) {
+	var state = {};
+	state[key] = value;
+	this.setState(state);
+  }
+
   render() {
 	var time = this.state.time;
+	console.log(this.state);
+	var pauseResume = (this.state.running) ? 'Pause' : 'Resume';
+	var startWork = (this.state.isWork) ? 'Restart' : 'Start work.';
+	var stateClass = (this.state.isWork) ? 'work' : 'break';
 	return (
-	  <div>
-		<Timer time={time}/> 
-		<TimeEditor onReset={(newTime) => {this.setState({time: newTime*60});}}/>
+	  <div className={stateClass}>
+		<TimeComp time={this.state.timeInMillis}/>
+		<button onClick={()=>this.pauseResumeClicked()}>
+		  {pauseResume}
+        </button>
+		<button onClick={()=>this.startWork()}>
+		  {startWork}
+		</button>
+		<TimeEditor label="Work" 
+	                workTime={this.state.formWork}
+	                breakTime={this.state.formBreak}
+  	                setWork={(value) => {this.setState({formWork: value});}}
+  	                setBreak={(value) => {this.setState({formBreak: value});}}
+	                onReset={this.reset.bind(this)} 
+		/>
 	  </div>
 	);
   }
 }
 
 
+// Notifications
 var supportNotification = "Notification" in window;
 function maybeGetPermission() {
   if (!supportNotification) {
@@ -208,6 +264,7 @@ function notify(onclick) {
   }
 }
 
+// Render
 ReactDOM.render(
   <App />,
   document.getElementById('container')
